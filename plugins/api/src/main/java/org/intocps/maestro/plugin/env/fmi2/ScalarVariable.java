95a56d36f9622af87f790cb9bda14481910348ed
Kenneth Lausdahl <kgl@mjolner.dk>
2020-05-11 14:39:33 +0200

    added initial plugin env structure

---
+++ plugins/api/src/main/java/org/intocps/maestro/plugin/env/fmi2/ScalarVariable.java
@@ -0,0 +1,7 @@
+package org.intocps.maestro.plugin.env.fmi2;
+
+import org.intocps.maestro.plugin.env.UnitRelationship;
+
+public class ScalarVariable implements UnitRelationship.FrameworkVariableInfo {
+    final int valueReference = 0;
+}
