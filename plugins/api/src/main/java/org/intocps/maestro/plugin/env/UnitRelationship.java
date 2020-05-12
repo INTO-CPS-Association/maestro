230115d6e49a1ab47a8c633a31165ddf736a0d92
Casper Thule <casper.thule@eng.au.dk>
2020-05-12 09:12:31 +0200

    Created unit relationships

--- plugins/api/src/main/java/org/intocps/maestro/plugin/env/UnitRelationship.java
+++ plugins/api/src/main/java/org/intocps/maestro/plugin/env/UnitRelationship.java
@@ -1,14 +1,20 @@
 package org.intocps.maestro.plugin.env;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.intocps.fmi.IFmu;
 import org.intocps.maestro.ast.LexIdentifier;
 import org.intocps.maestro.core.Framework;
 import org.intocps.maestro.plugin.env.fmi2.ComponentInfo;
 import org.intocps.maestro.plugin.env.fmi2.RelationVariable;
+import org.intocps.orchestration.coe.FmuFactory;
 import org.intocps.orchestration.coe.config.ModelConnection;
 import org.intocps.orchestration.coe.modeldefinition.ModelDescription;
 
 import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -19,84 +25,125 @@ public class UnitRelationship implements ISimulationEnvironment {
     Map<LexIdentifier, Set<Relation>> variableToRelations = new HashMap<>();
     Map<String, ComponentInfo> instanceNameToInstanceComponentInfo = new HashMap<>();
 
-    //TODO: Provide constructor that takes json inputstream.
-    public UnitRelationship(List<ModelConnection> connections, HashMap<String, ModelDescription> fmuKeyToModelDescription) throws IllegalAccessException, XPathExpressionException, InvocationTargetException, EnvironmentException {
+    public UnitRelationship(InputStream is)
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        EnvironmentMessage msg = null;
+        try {
+            msg = mapper.readValue(is, EnvironmentMessage.class);
+            Map<String, URI> fmuToURI = msg.getFmuFiles();
+            List<ModelConnection> connections = buildConnections(msg.connections);
+            HashMap<String, ModelDescription> fmuKeyToModelDescription = buildFmuKeyToFmuMD(fmuToURI);
 
-        Set<ModelConnection.ModelInstance> instancesFromConnections = new HashSet<>();
-        for(ModelConnection.ModelInstance instance : instancesFromConnections){
-            instanceNameToInstanceComponentInfo.put(instance.instanceName, new ComponentInfo(fmuKeyToModelDescription.get(instance.key)));
-        }
+            Set<ModelConnection.ModelInstance> instancesFromConnections = new HashSet<>();
+            for(ModelConnection instance : connections){
+                instancesFromConnections.add(instance.from.instance);
+                instancesFromConnections.add(instance.to.instance);
+                if(!instanceNameToInstanceComponentInfo.containsKey(instance.from.instance.instanceName))
+                    instanceNameToInstanceComponentInfo.put(instance.from.instance.instanceName, new ComponentInfo(fmuKeyToModelDescription.get(instance.from.instance.key)));
+                if(!instanceNameToInstanceComponentInfo.containsKey(instance.to.instance.instanceName))
+                    instanceNameToInstanceComponentInfo.put(instance.to.instance.instanceName, new ComponentInfo(fmuKeyToModelDescription.get(instance.to.instance.key)));
+            }
 
-        for(ModelConnection.ModelInstance instance : instancesFromConnections )
-        {
-            LexIdentifier instanceLexIdentifier = new LexIdentifier(instance.instanceName, null);
-            Set<Relation> instanceRelations = getOrCrateRelationsForLexIdentifier(instanceLexIdentifier);
-
-            List<ModelDescription.ScalarVariable> instanceOutputScalarVariablesPorts =
-                    instanceNameToInstanceComponentInfo.get(instance.key).modelDescription.getScalarVariables().stream()
-                            .filter(x -> x.causality == ModelDescription.Causality.Output).collect(Collectors.toList());
-
-            for(ModelDescription.ScalarVariable outputScalarVariable : instanceOutputScalarVariablesPorts)
+            for(ModelConnection.ModelInstance instance : instancesFromConnections )
             {
-                Variable outputVariable = new Variable(new RelationVariable(outputScalarVariable, instanceLexIdentifier));
+                LexIdentifier instanceLexIdentifier = new LexIdentifier(instance.instanceName, null);
+                Set<Relation> instanceRelations = getOrCrateRelationsForLexIdentifier(instanceLexIdentifier);
 
-                // dependantInputs are the inputs on which the current output depends on internally
-                Map<LexIdentifier, Variable> dependantInputs = new HashMap<>();
-                for(ModelDescription.ScalarVariable inputScalarVariable : outputScalarVariable.outputDependencies.keySet()){
-                    Variable inputVariable = new Variable(new RelationVariable(inputScalarVariable, instanceLexIdentifier));
-                    dependantInputs.put(instanceLexIdentifier, inputVariable);
-                    // TODO: Add relation from each input to the given output?
-                }
-                if(dependantInputs.size() != 0){
-                    Relation r = new Relation();
-                    r.source = outputVariable;
-                    r.targets = dependantInputs;
-                    r.direction = Relation.Direction.OutputToInput;
-                    r.internalOrExternal = Relation.InternalOrExternal.Internal;
-                    instanceRelations.add(r);
-                }
+                List<ModelDescription.ScalarVariable> instanceOutputScalarVariablesPorts =
+                        instanceNameToInstanceComponentInfo.get(instance.instanceName).modelDescription.getScalarVariables().stream()
+                                .filter(x -> x.causality == ModelDescription.Causality.Output).collect(Collectors.toList());
 
-                // externalInputTargets are the inputs that depends on the current output based on the provided connections.
-                List<ModelConnection.Variable> externalInputTargets = connections.stream().filter(conn -> conn.from.instance.equals(instance) && conn.from.variable.equals(outputScalarVariable.name)).map(conn -> conn.to).collect(Collectors.toList());
-                if(externalInputTargets.size() != 0)
+                for(ModelDescription.ScalarVariable outputScalarVariable : instanceOutputScalarVariablesPorts)
                 {
-                    // externalInputs are all the external Inputs that depends on the current output
-                    Map<LexIdentifier, Variable> externalInputs = new HashMap<>();
-                    for(ModelConnection.Variable modelConnToVar : externalInputTargets)
-                    {
-                        ModelDescription md = instanceNameToInstanceComponentInfo.get(modelConnToVar.instance.key).modelDescription;
-                        Optional<ModelDescription.ScalarVariable> toScalarVariable = md.getScalarVariables().stream().filter(sv -> sv.name.equals(modelConnToVar.variable)).findFirst();
-                        if(toScalarVariable.isPresent())
-                        {
-                            LexIdentifier inputInstanceLexIdentifier = new LexIdentifier(modelConnToVar.instance.instanceName, null);
-                            Variable inputVariable = new Variable(new RelationVariable(toScalarVariable.get(), inputInstanceLexIdentifier));
-                            externalInputs.put(inputInstanceLexIdentifier, inputVariable);
+                    Variable outputVariable = new Variable(new RelationVariable(outputScalarVariable, instanceLexIdentifier));
 
-                            //Add relation from the input to the given output
-                            Set<Relation> inputInstanceRelations = getOrCrateRelationsForLexIdentifier(instanceLexIdentifier);
-                            Relation r = new Relation();
-                            r.source = inputVariable;
-                            r.targets = new HashMap<LexIdentifier, Variable>(){{
-                                put(inputInstanceLexIdentifier, inputVariable);
-                            }};
-                            r.internalOrExternal = Relation.InternalOrExternal.External;
-                            r.direction = Relation.Direction.InputToOutput;
-                            inputInstanceRelations.add(r);
-                        }
-                        {
-                            throw new EnvironmentException("Failed to find the scalar variable " + modelConnToVar.variable + " at " + modelConnToVar.instance + " when building the dependencies tree");
-                        }
+                    // dependantInputs are the inputs on which the current output depends on internally
+                    Map<LexIdentifier, Variable> dependantInputs = new HashMap<>();
+                    for(ModelDescription.ScalarVariable inputScalarVariable : outputScalarVariable.outputDependencies.keySet()){
+                        Variable inputVariable = new Variable(new RelationVariable(inputScalarVariable, instanceLexIdentifier));
+                        dependantInputs.put(instanceLexIdentifier, inputVariable);
+                        // TODO: Add relation from each input to the given output?
+                    }
+                    if(dependantInputs.size() != 0){
+                        Relation r = new Relation();
+                        r.source = outputVariable;
+                        r.targets = dependantInputs;
+                        r.direction = Relation.Direction.OutputToInput;
+                        r.internalOrExternal = Relation.InternalOrExternal.Internal;
+                        instanceRelations.add(r);
                     }
 
-                    Relation r = new Relation();
-                    r.source = outputVariable;
-                    r.targets = externalInputs;
-                    r.direction = Relation.Direction.OutputToInput;
-                    r.internalOrExternal = Relation.InternalOrExternal.External;
-                    instanceRelations.add(r);
+                    // externalInputTargets are the inputs that depends on the current output based on the provided connections.
+                    List<ModelConnection.Variable> externalInputTargets = connections.stream().filter(conn -> conn.from.instance.equals(instance) && conn.from.variable.equals(outputScalarVariable.name)).map(conn -> conn.to).collect(Collectors.toList());
+                    if(externalInputTargets.size() != 0)
+                    {
+                        // externalInputs are all the external Inputs that depends on the current output
+                        Map<LexIdentifier, Variable> externalInputs = new HashMap<>();
+                        for(ModelConnection.Variable modelConnToVar : externalInputTargets)
+                        {
+                            ModelDescription md = instanceNameToInstanceComponentInfo.get(modelConnToVar.instance.instanceName).modelDescription;
+                            Optional<ModelDescription.ScalarVariable> toScalarVariable = md.getScalarVariables().stream().filter(sv -> sv.name.equals(modelConnToVar.variable)).findFirst();
+                            if(toScalarVariable.isPresent())
+                            {
+                                LexIdentifier inputInstanceLexIdentifier = new LexIdentifier(modelConnToVar.instance.instanceName, null);
+                                Variable inputVariable = new Variable(new RelationVariable(toScalarVariable.get(), inputInstanceLexIdentifier));
+                                externalInputs.put(inputInstanceLexIdentifier, inputVariable);
+
+                                //Add relation from the input to the given output
+                                Set<Relation> inputInstanceRelations = getOrCrateRelationsForLexIdentifier(inputInstanceLexIdentifier);
+                                Relation r = new Relation();
+                                r.source = inputVariable;
+                                r.targets = new HashMap<LexIdentifier, Variable>(){{
+                                    put(instanceLexIdentifier, outputVariable);
+                                }};
+                                r.internalOrExternal = Relation.InternalOrExternal.External;
+                                r.direction = Relation.Direction.InputToOutput;
+                                inputInstanceRelations.add(r);
+                            }
+                            else {
+                                throw new EnvironmentException("Failed to find the scalar variable " + modelConnToVar.variable + " at " + modelConnToVar.instance + " when building the dependencies tree");
+                            }
+                        }
+
+                        Relation r = new Relation();
+                        r.source = outputVariable;
+                        r.targets = externalInputs;
+                        r.direction = Relation.Direction.OutputToInput;
+                        r.internalOrExternal = Relation.InternalOrExternal.External;
+                        instanceRelations.add(r);
+                    }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public static List<ModelConnection> buildConnections(Map<String, List<String>> connections) throws Exception {
+        List<ModelConnection> list = new Vector<>();
+
+        for (Map.Entry<String, List<String>> entry : connections.entrySet()) {
+            for (String input : entry.getValue()) {
+                list.add(new ModelConnection(ModelConnection.Variable.parse(entry.getKey()), ModelConnection.Variable.parse(input)));
+            }
+        }
+
+        return list;
+    }
+
+    private HashMap<String, ModelDescription> buildFmuKeyToFmuMD(Map<String, URI> fmus) throws Exception {
+        HashMap<String, ModelDescription> fmuKeyToFmuWithMD = new HashMap<>();
+        for(Map.Entry<String, URI> entry : fmus.entrySet()) {
+            String key = entry.getKey();
+            URI value = entry.getValue();
+            IFmu fmu = FmuFactory.create(null,value);
+            ModelDescription md = new ModelDescription(fmu.getModelDescription());
+            fmuKeyToFmuWithMD.put(key, md);
+        }
+
+        return fmuKeyToFmuWithMD;
     }
 
     Set<Relation> getOrCrateRelationsForLexIdentifier(LexIdentifier instanceLexIdentifier){
@@ -121,12 +168,12 @@ public class UnitRelationship implements ISimulationEnvironment {
 
         // a, b
 
-        Map<LexIdentifier, Object> internalMapping = identifiers.stream().collect(Collectors.toMap(Function.identity(), id -> {
+//        Map<LexIdentifier, Object> internalMapping = identifiers.stream().collect(Collectors.toMap(Function.identity(), id -> {
 
             //check context to find which FMU in the internal map that id points to in terms of FMU.modelinstance
-            return null;
+//            return null;
 
-        }));
+//        }));
 
         //a -> FMUA, b-> FMUB
 
@@ -145,7 +192,7 @@ public class UnitRelationship implements ISimulationEnvironment {
         /*
          * user of this for stepping will first look for all outputs from here and collect these or directly set or use these outputs + others and
          * then use the relation to set these*/
-        Set<Relation> returnValues = identifiers.stream().map(lexId -> variableToRelations.get(lexId.getText())).flatMap(x -> x.stream()).collect(Collectors.toSet());
+        Set<Relation> returnValues = identifiers.stream().map(lexId -> variableToRelations.get(lexId)).flatMap(x -> x.stream()).collect(Collectors.toSet());
         return returnValues;
     }
 
