9ace10a9f8de8f50165572742dac478d278a710f
Simon Thrane <sthranehansen@gmail.com>
2020-06-17 13:47:57 +0200

    Updated scala versions

--- plugins/initializer2/src/main/java/org/intocps/maestro/plugin/InitializerNew/TopologicalPlugin.java
+++ plugins/initializer2/src/main/java/org/intocps/maestro/plugin/InitializerNew/TopologicalPlugin.java
@@ -6,11 +6,9 @@ import org.intocps.topologicalsorting.TarjanGraph;
 import org.intocps.topologicalsorting.data.AcyclicDependencyResult;
 import org.intocps.topologicalsorting.data.CyclicDependencyResult;
 import org.intocps.topologicalsorting.data.Edge11;
-import scala.jdk.CollectionConverters;
+import scala.collection.JavaConverters;
 
-import java.util.List;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TopologicalPlugin {
@@ -30,14 +28,13 @@ public class TopologicalPlugin {
             edges.add(new Edge11(e, o.getSource(), o.getOrigin()));
         }));
 
-        var graphSolver = new TarjanGraph(CollectionConverters.IterableHasAsScala(edges).asScala());
+        var graphSolver = new TarjanGraph(JavaConverters.iterableAsScalaIterableConverter(edges).asScala());
 
         var topologicalOrderToInstantiate = graphSolver.topologicalSort();
         if (topologicalOrderToInstantiate instanceof CyclicDependencyResult) {
             CyclicDependencyResult cycles = (CyclicDependencyResult) topologicalOrderToInstantiate;
             throw new UnfoldException("Cycles are present in the systems: " + cycles.cycle());
         }
-
-        return scala.jdk.javaapi.CollectionConverters.asJava(((AcyclicDependencyResult) topologicalOrderToInstantiate).totalOrder());
+        return (List<UnitRelationship.Variable>) JavaConverters.seqAsJavaListConverter(((AcyclicDependencyResult) topologicalOrderToInstantiate).totalOrder()).asJava();
     }
 }
--- plugins/topologicalsorting/src/main/scala/org/intocps/topologicalsorting/data/DependencyResult.scala
+++ plugins/topologicalsorting/src/main/scala/org/intocps/topologicalsorting/data/DependencyResult.scala
@@ -2,4 +2,4 @@ package org.intocps.topologicalsorting.data
 
 sealed trait DependencyResult[A]
 case class CyclicDependencyResult[A](cycle: String) extends DependencyResult[A]
-case class AcyclicDependencyResult[A](totalOrder : List[A]) extends DependencyResult[A]
+case class AcyclicDependencyResult[A](totalOrder : Seq[A]) extends DependencyResult[A]
