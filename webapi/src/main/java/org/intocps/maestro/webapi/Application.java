8704c7381a9fee313acb74ffc126c750f5e60541
Kenneth Lausdahl <kgl@mjolner.dk>
2020-10-22 22:22:14 +0200

    added new v1 main and moved temp folder to a temp location

--- webapi/src/main/java/org/intocps/maestro/webapi/Application.java
+++ webapi/src/main/java/org/intocps/maestro/webapi/Application.java
@@ -1,19 +1,63 @@
 package org.intocps.maestro.webapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.cli.CommandLine;
+import org.intocps.maestro.MaestroV1CliProxy;
+import org.intocps.maestro.webapi.maestro2.Maestro2Broker;
+import org.intocps.maestro.webapi.maestro2.Maestro2SimulationController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    final static ObjectMapper mapper = new ObjectMapper();
 
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8082"));
-        app.run(args);
-        //        SpringApplication.run(Application.class, args);
+        CommandLine cmd = MaestroV1CliProxy.parse(args);
+
+        if (cmd.hasOption(MaestroV1CliProxy.oneShotOpt.getOpt()) || cmd.hasOption(MaestroV1CliProxy.portOpt.getOpt()) ||
+                cmd.hasOption(MaestroV1CliProxy.versionOpt.getOpt())) {
+
+            MaestroV1CliProxy.process(cmd, new MableV1ToV2ProxyRunner(), port -> {
+                SpringApplication app = new SpringApplication(Application.class);
+                app.run("--server.port=" + port);
+            });
+            return;
+        }
+        SpringApplication.run(Application.class, args);
+    }
+
+    static class MableV1ToV2ProxyRunner implements MaestroV1CliProxy.OneShotRunner {
+
+        @Override
+        public boolean run(boolean verbose, File configFile, File simulationConfigFile, Double startTime, Double endTime,
+                File outputFile) throws IOException {
+            Maestro2SimulationController.InitializationData initializationData =
+                    mapper.readValue(configFile, Maestro2SimulationController.InitializationData.class);
+
+            Maestro2SimulationController.SimulateRequestBody simulationData = null;
+            if (simulationConfigFile != null && simulationConfigFile.exists()) {
+                simulationData = mapper.readValue(configFile, Maestro2SimulationController.SimulateRequestBody.class);
+            } else {
+                simulationData = new Maestro2SimulationController.SimulateRequestBody(startTime, endTime, new HashMap<>(), false, 0d);
+            }
+
+            File workingDirectory = Paths.get("").toFile();
+            Maestro2Broker mc = new Maestro2Broker(workingDirectory);
+            try {
+                mc.build(initializationData, simulationData, null);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
     }
 }
