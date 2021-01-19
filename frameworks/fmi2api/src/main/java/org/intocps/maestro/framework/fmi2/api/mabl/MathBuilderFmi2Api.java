f6f6f21a9a067d6892bae076752c9b7f7d6f4276
Casper Thule <casper.thule@eng.au.dk>
2021-01-19 15:20:58 +0100

    Created generic add

--- frameworks/fmi2api/src/main/java/org/intocps/maestro/framework/fmi2/api/mabl/MathBuilderFmi2Api.java
+++ frameworks/fmi2api/src/main/java/org/intocps/maestro/framework/fmi2/api/mabl/MathBuilderFmi2Api.java
@@ -1,14 +1,12 @@
 package org.intocps.maestro.framework.fmi2.api.mabl;
 
-import org.intocps.maestro.ast.node.PStm;
 import org.intocps.maestro.framework.fmi2.api.Fmi2Builder;
 
 import static org.intocps.maestro.ast.MableAstFactory.newPlusExp;
 
 public class MathBuilderFmi2Api {
 
-    public static Fmi2Builder.ProvidesReferenceExp add(Fmi2Builder.DoubleVariable<PStm> currentCommunicationTime,
-            Fmi2Builder.DoubleVariable<PStm> stepSizeVar) {
-        return new ExpFmi2Api(newPlusExp(currentCommunicationTime.getReferenceExp(), stepSizeVar.getReferenceExp()));
+    public static Fmi2Builder.ProvidesReferenceExp add(Fmi2Builder.ProvidesReferenceExp left, Fmi2Builder.ProvidesReferenceExp right) {
+        return new ExpFmi2Api(newPlusExp(left.getReferenceExp(), right.getReferenceExp()));
     }
 }
