4c26dfbfc75536e28cb6a5d1d566a6b0c9baa9f7
Casper Thule <casper.thule@eng.au.dk>
2021-02-05 13:53:48 +0100

    windows issue

--- maestro/src/test/java/org/intocps/maestro/Issue188Test.java
+++ maestro/src/test/java/org/intocps/maestro/Issue188Test.java
@@ -1,11 +1,12 @@
 package org.intocps.maestro;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Issue188Test {
     @Test
     public void InterpretSpec() throws Exception {
-        Main.argumentHandler(new String[]{"-i", Issue188Test.class.getClassLoader().getResource("188/spec.mabl").getPath()});
+        Assert.assertTrue(Main.argumentHandler(new String[]{"-i", Issue188Test.class.getClassLoader().getResource("188/spec.mabl").getPath()}));
     }
 
     @Test
@@ -13,7 +14,7 @@ public class Issue188Test {
         String initializeJson = Issue188Test.class.getClassLoader().getResource("188/initialize.json").getPath();
         String simulateJson = Issue188Test.class.getClassLoader().getResource("188/simulate.json").getPath();
         String dumpPath = "target/test-classes/188/dump";
-        Main.argumentHandler(new String[]{"-sg1", initializeJson, simulateJson, "-d", dumpPath});
+        Assert.assertTrue(Main.argumentHandler(new String[]{"-sg1", initializeJson, simulateJson, "-d", dumpPath}));
     }
 
 }
