67f6a951849cdd2393a6f79f396f7c12a41cbf2a
Max <max.rose366@gmail.com>
2021-02-14 19:44:34 +0000

    finished test

--- maestro-webapi/src/test/java/org/intocps/maestro/webapi/maestro2/TestSimultaneousSimulations.java
+++ maestro-webapi/src/test/java/org/intocps/maestro/webapi/maestro2/TestSimultaneousSimulations.java
@@ -1,19 +1,16 @@
 package org.intocps.maestro.webapi.maestro2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.compress.archivers.zip.ZipUtil;
 import org.apache.commons.io.FileUtils;
-import org.intocps.fmi.jnifmuapi.ZipUtility;
-import org.intocps.maestro.core.ZipUtilities;
 import org.intocps.maestro.webapi.maestro2.dto.InitializeStatusModel;
 import org.intocps.maestro.webapi.maestro2.dto.StatusModel;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
+import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -25,15 +22,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -49,7 +43,7 @@ public class TestSimultaneousSimulations {
     @Autowired
     private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
-    private File simulationJson, initJson;
+    private File simulationJson, initJson, expectedOutputCSV;
 
     @BeforeEach
     public void before()
@@ -59,14 +53,43 @@ public class TestSimultaneousSimulations {
 
         simulationJson = new File(TestSimultaneousSimulations.class.getClassLoader().getResource("maestro2/ThreadedTest/simulate.json").getPath());
         initJson = new File(TestSimultaneousSimulations.class.getClassLoader().getResource("maestro2/ThreadedTest/initialize.json").getPath());
+        expectedOutputCSV = new File(TestSimultaneousSimulations.class.getClassLoader().getResource("maestro2/ThreadedTest/outputs.csv").getPath());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {2})
-    public void TestMultithreadedPerformance(int threads) throws Exception
+    @ValueSource(ints = {1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20})
+    public void TestMultithreadedPerformance(int threadCount) throws Exception
     {
-        RunSingleSimulation();
-        Assertions.fail();
+        var threads = new ArrayList<Thread>();
+        final AssertionFailedError[] failedTest = new AssertionFailedError[1];
+
+        for(var t = 0; t < threadCount; t++)
+        {
+            var thread = new Thread() {
+                public void run() {
+                    try {
+                        RunSingleSimulation();
+                    } catch (AssertionFailedError f)
+                    {
+                        failedTest[0] = f; // spcificaly catch assertion erorrs since junit cant throw from subthreads
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            threads.add(thread);
+
+            thread.start();
+        }
+
+        while(threads.size() != 0)
+        {
+            threads.remove(0).join();
+            if(failedTest[0] != null)
+                throw failedTest[0]; // rethrow any failed tests so that the test actually fail
+        }
     }
 
     private void RunSingleSimulation() throws Exception
@@ -94,15 +117,55 @@ public class TestSimultaneousSimulations {
                 mockMvc.perform(get("/result/" + sessionId + "/zip")).andExpect(status().is(HttpStatus.OK.value())).andReturn().getResponse().getContentAsByteArray();
         Assertions.assertNotNull(zippedResult);
 
+        var tempZipDir = Files.createTempDirectory("testZip"); // need a place to extract the zip to to ensure that the FMU was executed properly
+        tempZipDir.toFile().deleteOnExit(); // means the folder and content will be deleted no matter if we crash or not
         var iStream = new ZipInputStream(new ByteArrayInputStream(zippedResult));
         var entries = new ArrayList<ZipEntry>();
         ZipEntry entry;
 
         while((entry = iStream.getNextEntry()) != null)
+        {
             entries.add(entry);
+            WriteZipContent(iStream, entry, tempZipDir.toFile());
+        }
+        iStream.closeEntry();
+        iStream.close();
 
         var zipEntryNames = entries.stream().map(ZipEntry::getName).collect(Collectors.toList());
-        assertThat(zipEntryNames).containsExactlyInAnyOrder("initialize.json", "simulate.json", "spec.mabl", "outputs.csv", "spec.runtime.json","equations.log");
+        assertThat(zipEntryNames)
+                .containsExactlyInAnyOrder("initialize.json", "simulate.json", "spec.mabl", "outputs.csv", "spec.runtime.json", "equations.log");
+        CheckZipFileContent(tempZipDir.toFile());
+
+        mockMvc.perform(get("/destroy/" + sessionId)).andExpect(status().is(HttpStatus.OK.value()));
+    }
+
+    private void CheckZipFileContent(File extractedPath) throws IOException
+    {
+        // TODO: Possibly look into a nicer way of doing this
+
+        for(File f : extractedPath.listFiles())
+        {
+            if(f.getName().equals("outputs.csv"))
+            {
+                var f1 = new HashSet<>(FileUtils.readLines(f));
+                var f2 = new HashSet<>(FileUtils.readLines(expectedOutputCSV));
+
+                Assertions.assertEquals(f2.size(), f1.size(), "Not enough entries in results file!");
+            }
+        }
+    }
+
+    private void WriteZipContent(ZipInputStream zipInputStream, ZipEntry entry, File unzipPath) throws Exception
+    {
+        var newFile = new File(unzipPath, entry.getName());
+        newFile.deleteOnExit();
+
+        var buffer = new byte[1024];
+        var fos = new FileOutputStream(newFile);
+        int len;
+        while((len = zipInputStream.read(buffer)) > 0)
+            fos.write(buffer, 0, len);
+        fos.close();
     }
 
     private void CheckSimpleResponse(String expectedStatus, String sessionId, StatusModel reponse)
