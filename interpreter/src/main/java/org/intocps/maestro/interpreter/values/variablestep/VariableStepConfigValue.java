fe855dc8132b000491d5529c3ee3950ef18e88c0
FrederikPM <frdrkpm@gmail.com>
2021-02-18 10:46:52 +0100

    Implemented getStepSize for variable step

--- interpreter/src/main/java/org/intocps/maestro/interpreter/values/variablestep/VariableStepConfigValue.java
+++ interpreter/src/main/java/org/intocps/maestro/interpreter/values/variablestep/VariableStepConfigValue.java
@@ -1,37 +1,71 @@
 package org.intocps.maestro.interpreter.values.variablestep;
 
 import org.intocps.maestro.interpreter.values.Value;
+import org.intocps.orchestration.coe.AbortSimulationException;
 import org.intocps.orchestration.coe.config.ModelConnection;
 import org.intocps.orchestration.coe.cosim.base.FmiSimulationInstance;
 import org.intocps.orchestration.coe.modeldefinition.ModelDescription;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class VariableStepConfigValue extends Value {
 
     private final Map<ModelConnection.ModelInstance, FmiSimulationInstance> instances;
     private List<String> portNames;
+    private Map<ModelDescription.Types, StepVal> dataPoint;
+    private Double time;
+    private StepsizeCalculator stepsizeCalculator;
 
-    public VariableStepConfigValue(Map<ModelConnection.ModelInstance, FmiSimulationInstance> instances) {
+    public VariableStepConfigValue(Map<ModelConnection.ModelInstance, FmiSimulationInstance> instances,
+            Set<InitializationMsgJson.Constraint> constraints,
+            StepsizeInterval stepsizeInterval,
+            Double initSize) throws AbortSimulationException {
         this.instances = instances;
+        stepsizeCalculator = new StepsizeCalculator(constraints, stepsizeInterval, initSize, instances);
     }
 
     public void initializePorts(List<String> portNames) {
         this.portNames = portNames;
     }
 
-    public void addDataPoint(Double time, Map<ModelDescription.Types, Object> dataPoint) {
+    public void addDataPoint(Double time, Map<ModelDescription.Types, StepVal> dataPoint) {
+        this.time = time;
+        this.dataPoint = dataPoint;
     }
 
     public double getStepSize() {
-        return 0;
+        Map<ModelConnection.ModelInstance, Map<ModelDescription.ScalarVariable, Object>> currentValues = new HashMap<>();
+        instances.forEach((mi,si) -> {
+            Map<ModelDescription.ScalarVariable, Object> scalarVals = new HashMap<>();
+            si.config.scalarVariables.forEach( sv -> {
+                Object value = dataPoint.values().stream().filter(dp -> (dp.getName().contains(mi.key) && dp.getName().contains(sv.name))).findFirst();
+                if(value != null)
+                    scalarVals.put(sv, value);
+            });
+            currentValues.put(mi, scalarVals);
+        });
+
+        return stepsizeCalculator.getStepsize(time,currentValues, null, null);
     }
 
-    public static class VarStepValue {
-        public String name;
-        public Object value;
+    public List<String> getPorts() { return this.portNames; }
 
+    public static class StepVal {
+        private String name;
+        private Object value;
+
+        public StepVal(String name, Object value){
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() { return name; }
+        public Object getValue() { return value; }
+        public void setName(String name) { this.name = name; }
+        public void setValue(Object value) { this.value = value; }
     }
 
 
--- interpreter/src/main/java/org/intocps/maestro/interpreter/values/variablestep/VariableStepValue.java
+++ interpreter/src/main/java/org/intocps/maestro/interpreter/values/variablestep/VariableStepValue.java
@@ -1,5 +1,6 @@
 package org.intocps.maestro.interpreter.values.variablestep;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.intocps.fmi.IFmu;
 import org.intocps.maestro.interpreter.InterpreterException;
 import org.intocps.maestro.interpreter.values.*;
@@ -10,24 +11,74 @@ import org.intocps.orchestration.coe.cosim.base.FmiInstanceConfig;
 import org.intocps.orchestration.coe.cosim.base.FmiSimulationInstance;
 import org.intocps.orchestration.coe.modeldefinition.ModelDescription;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class VariableStepValue extends ModuleValue {
 
-    public VariableStepValue(String configuration) {
-        super(createMembers());
-        //        ObjectMapper om = new ObjectMapper();
-        //        ConstraintsJson constraintsJson = om.readValue(configuration, ConstraintsJson.class);
-        //        constraintsJson.constraints
-
+    public VariableStepValue(String configuration){
+        super(createMembers(configuration));
     }
 
+    private static Set<InitializationMsgJson.Constraint> getConstraintsFromConfig(InitializationMsgJson config){
+        final Set<InitializationMsgJson.Constraint> constraints = new HashSet<>();
+        Object constraintValues = config.algorithm.get("constraints");
+        Map<String, Map<String, Object>> namedConstraints = (Map<String, Map<String, Object>>) constraintValues;
 
-    private static Map<String, Value> createMembers() {
+        for (Map.Entry<String, Map<String, Object>> entry : namedConstraints.entrySet()) {
+            final InitializationMsgJson.Constraint constraint = InitializationMsgJson.Constraint.parse(entry.getValue());
+            constraint.setId(entry.getKey());
+            constraints.add(constraint);
+        }
+        return constraints;
+    }
+
+    private static StepsizeInterval getStepsizeIntervalFromConfig(InitializationMsgJson config) {
+        List<Double> size = new Vector<>();
+        Object objSize = config.algorithm.get("size");
+        for (Object number : (List<Object>) objSize) {
+            if (number instanceof Integer) {
+                number = new Double((Integer) number);
+            }
+
+            if (number instanceof Double) {
+                size.add((Double) number);
+            }
+        }
+        return new StepsizeInterval(size.get(0), size.get(1));
+    }
+
+    private static Double getInitSizeFromConfig(InitializationMsgJson config){
+        Double initsize = -1.0;
+        Object objInitsize = config.algorithm.get("initsize");
+        if (objInitsize instanceof Double) {
+            initsize = (Double) objInitsize;
+        } else if (objInitsize instanceof Integer) {
+            initsize = new Double((Integer) objInitsize);
+        }
+        return initsize;
+    }
+
+    /**
+     * Values passed in addDataPoint is assumed to follow the same order as the portNames passed in initializePortNames.
+     * @param configJson
+     * @return
+     */
+    private static Map<String, Value> createMembers(String configJson) {
+        Set<InitializationMsgJson.Constraint> constraints;
+        StepsizeInterval stepsizeInterval;
+        Double initSize;
+
+        try{
+            InitializationMsgJson config = (new ObjectMapper()).readValue(configJson, InitializationMsgJson.class);
+            constraints = getConstraintsFromConfig(config);
+            stepsizeInterval = getStepsizeIntervalFromConfig(config);
+            initSize = getInitSizeFromConfig(config);
+        }
+        catch (Exception e){
+            throw new InterpreterException("Configuration could not be passed", e);
+        }
+
         Map<String, Value> componentMembers = new HashMap<>();
         componentMembers.put("setFMUs", new FunctionValue.ExternalFunctionValue(fcargs -> {
             try {
@@ -39,20 +90,20 @@ public class VariableStepValue extends ModuleValue {
                     throw new InterpreterException("Argument list contains null values");
                 }
 
-                List<StringValue> fmuNames = ArrayUtilValue.getArrayValue(fcargs.get(1), StringValue.class);
+                List<StringValue> fmuInstanceNames = ArrayUtilValue.getArrayValue(fcargs.get(1), StringValue.class);
                 List<FmuValue> fmus = ArrayUtilValue.getArrayValue(fcargs.get(1), FmuValue.class);
 
                 Map<ModelConnection.ModelInstance, FmiSimulationInstance> instances = new HashMap<>();
 
-                for (int i = 0; i < fmuNames.size(); i++) {
-                    ModelConnection.ModelInstance mi = new ModelConnection.ModelInstance(fmuNames.get(i).getValue(), null);
+                for (int i = 0; i < fmuInstanceNames.size(); i++) {
+                    ModelConnection.ModelInstance mi = new ModelConnection.ModelInstance(fmuInstanceNames.get(i).getValue(), null);
                     IFmu fmu = fmus.get(i).getModule();
                     ModelDescription modelDescription = new ModelDescription(fmu.getModelDescription());
                     FmiInstanceConfig fmiInstanceConfig = new FmiInstanceConfig(modelDescription, modelDescription.getScalarVariables());
                     FmiSimulationInstance si = new FmiSimulationInstance(null, fmiInstanceConfig);
                     instances.put(mi, si);
                 }
-                return new VariableStepConfigValue(instances);
+                return new VariableStepConfigValue(instances, constraints, stepsizeInterval, initSize);
             } catch (Exception e) {
                 return null;
             }
@@ -84,28 +135,44 @@ public class VariableStepValue extends ModuleValue {
 
             Value id = fcargs.get(0).deref();
             if (id instanceof VariableStepConfigValue) {
-
+                VariableStepConfigValue variableStepConfigValue = (VariableStepConfigValue)id;
                 double time = ((RealValue) fcargs.get(1).deref()).getValue();
                 List<Value> arrayValue = fcargs.stream().skip(2).map(Value::deref).collect(Collectors.toList());
-                Map<ModelDescription.Types, Object> typesToValues = new HashMap<>();
-                arrayValue.forEach(x -> {
-                    // TODO: These needs to be affiliated with portnames. Perhaps in the VariableStepConfigValue.VarStepValue?
-                    if (x instanceof StringValue) {
-                        typesToValues.put(ModelDescription.Types.String, ((StringValue) x).getValue());
-                    }
-                    // TODO: Int, real, boolean, enumeration
-                });
-                ((VariableStepConfigValue) id).addDataPoint(time, typesToValues);
-            }
+                Map<ModelDescription.Types, VariableStepConfigValue.StepVal> typesToValues = new HashMap<>();
 
+                //TODO: Should this type conversion and linking of port names and values be done in VariableStepConfig instead?
+                for(int i = 0; i < arrayValue.size(); i++) {
+                    Object value = arrayValue.get(i);
+
+                    if (value instanceof StringValue) {
+                        typesToValues.put(ModelDescription.Types.String, new VariableStepConfigValue.StepVal(variableStepConfigValue.getPorts().get(i), ((StringValue) value).getValue()));
+                    }
+                    else if (value instanceof IntegerValue) {
+                        typesToValues.put(ModelDescription.Types.Integer, new VariableStepConfigValue.StepVal(variableStepConfigValue.getPorts().get(i),
+                                ((IntegerValue) value).getValue()));
+                    }
+                    else if (value instanceof RealValue) {
+                        typesToValues.put(ModelDescription.Types.Real, new VariableStepConfigValue.StepVal(variableStepConfigValue.getPorts().get(i),
+                                ((RealValue) value).getValue()));
+                    }
+                    else if (value instanceof BooleanValue) {
+                        typesToValues.put(ModelDescription.Types.Boolean, new VariableStepConfigValue.StepVal(variableStepConfigValue.getPorts().get(i),
+                                ((BooleanValue) value).getValue()));
+                    }
+                    else if (value instanceof EnumerationValue) {
+                        typesToValues.put(ModelDescription.Types.Enumeration, new VariableStepConfigValue.StepVal(variableStepConfigValue.getPorts().get(i),
+                                ((EnumerationValue) value).getValue()));
+                    }
+
+                }
+                variableStepConfigValue.addDataPoint(time, typesToValues);
+            }
             return new VoidValue();
         }));
         componentMembers.put("getStepSize", new FunctionValue.ExternalFunctionValue(fcargs -> {
             Value id = fcargs.get(0).deref();
             if (id instanceof VariableStepConfigValue) {
-
-                VariableStepConfigValue id_ = (VariableStepConfigValue) id;
-                return new RealValue(id_.getStepSize());
+                return new RealValue(((VariableStepConfigValue) id).getStepSize());
             }
             return new RealValue(-1.0);
         }));
