2b84a6316c764ab174f2fe8e1608960532eb0dc2
FrederikPM <frdrkpm@gmail.com>
2021-03-01 08:01:46 +0100

    Spec check test runs

--- maestro/src/test/java/org/intocps/maestro/InterpreterTest.java
+++ maestro/src/test/java/org/intocps/maestro/InterpreterTest.java
@@ -1,14 +1,14 @@
-import org.antlr.v4.runtime.CharStreams;
+package org.intocps.maestro;
+
 import org.apache.commons.io.FileUtils;
 import org.intocps.maestro.ast.analysis.AnalysisException;
-import org.intocps.maestro.ast.node.ARootDocument;
-import org.intocps.maestro.parser.MablParserUtil;
+import org.intocps.maestro.framework.fmi2.api.mabl.BaseApiTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -17,21 +17,20 @@ import java.util.Objects;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
-
 @RunWith(Parameterized.class)
-public class StatementsTest {
+public class InterpreterTest extends BaseApiTest {
 
     final String spec;
     private final String name;
 
-    public StatementsTest(String name, String spec) {
+    public InterpreterTest(String name, String spec) {
         this.name = name;
         this.spec = spec;
     }
 
     @Parameterized.Parameters(name = "{index} \"{0}\"")
     public static Collection<Object> data() {
-        return Arrays.stream(Objects.requireNonNull(Paths.get("src", "test", "resources", "statements").toFile().listFiles())).map(f -> {
+        return Arrays.stream(Objects.requireNonNull(Paths.get("src", "test", "resources", "InterpreterTest" , "statements").toFile().listFiles())).map(f -> {
             try {
                 return new Object[]{f.getName(), FileUtils.readFileToString(f, StandardCharsets.UTF_8)};
             } catch (IOException e) {
@@ -42,15 +41,14 @@ public class StatementsTest {
     }
 
     @Test
-    public void test() throws IOException, AnalysisException {
+    public void test() throws IOException, AnalysisException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
         String templateSpec =
-                FileUtils.readFileToString(Paths.get("src", "test", "resources", "templates", "with-assert.mabl").toFile(), StandardCharsets.UTF_8);
+                FileUtils.readFileToString(Paths.get("src", "test", "resources", "InterpreterTest", "templates", "with-assert.mabl").toFile(),
+                        StandardCharsets.UTF_8);
 
         String aggrigatedSpec = templateSpec.replace("@replaceme", spec);
 
-        ARootDocument doc = MablParserUtil.parse(CharStreams.fromStream(new ByteArrayInputStream(aggrigatedSpec.getBytes())));
-
-
+        check(aggrigatedSpec,"");
     }
 }
