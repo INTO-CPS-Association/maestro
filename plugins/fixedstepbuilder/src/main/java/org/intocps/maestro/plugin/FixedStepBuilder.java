1887471690f80d3cb7cc9d800852a740e9299f6b
FrederikPM <frdrkpm@gmail.com>
2021-03-09 14:14:06 +0100

    Started on fixedStepBuilder

--- plugins/fixedstepbuilder/src/main/java/org/intocps/maestro/plugin/FixedStepBuilder.java
+++ plugins/fixedstepbuilder/src/main/java/org/intocps/maestro/plugin/FixedStepBuilder.java
@@ -216,6 +216,78 @@ public class FixedStepBuilder implements IMaestroExpansionPlugin {
         }
     }
 
+    public List<PStm> expand(AFunctionDeclaration declaredFunction, List<PExp> formalArguments, IPluginConfiguration config,
+            ISimulationEnvironment envIn, IErrorReporter errorReporter, MablApiBuilder builder) throws ExpandException {
+
+        FixedstepConfig fixedstepConfig = (FixedstepConfig) config;
+
+        if (!getDeclaredUnfoldFunctions().contains(declaredFunction)) {
+            throw new ExpandException("Unknown function declaration");
+        }
+        AFunctionDeclaration selectedFun = fun;
+
+        if (formalArguments == null || formalArguments.size() != selectedFun.getFormals().size()) {
+            throw new ExpandException("Invalid args");
+        }
+
+        if (envIn == null) {
+            throw new ExpandException("Simulation environment must not be null");
+        }
+
+        Fmi2SimulationEnvironment env = (Fmi2SimulationEnvironment) envIn;
+
+        PExp stepSize = formalArguments.get(1).clone();
+        PExp startTime = formalArguments.get(2).clone();
+        PExp endTime = formalArguments.get(3).clone();
+        if (declaredFunction.equals(fun)) {
+            try {
+                DynamicActiveBuilderScope dynamicScope = builder.getDynamicScope();
+
+                MathBuilderFmi2Api math = builder.getMablToMablAPI().getMathBuilder();
+                BooleanBuilderFmi2Api booleanLogic = builder.getMablToMablAPI().getBooleanBuilder();
+
+                // Convert raw MaBL to API
+                DoubleVariableFmi2Api externalStepSize = builder.getDoubleVariableFrom(stepSize);
+                DoubleVariableFmi2Api stepSizeVar = dynamicScope.store("fixed_step_size", 0.0);
+                stepSizeVar.setValue(externalStepSize);
+                DoubleVariableFmi2Api externalStartTime = new DoubleVariableFmi2Api(null, null, null, null, startTime);
+                DoubleVariableFmi2Api currentCommunicationTime = dynamicScope.store("fixed_current_communication_point", 0.0);
+                currentCommunicationTime.setValue(externalStartTime);
+                DoubleVariableFmi2Api externalEndTime = new DoubleVariableFmi2Api(null, null, null, null, endTime);
+                DoubleVariableFmi2Api endTimeVar = dynamicScope.store("fixed_end_time", 0.0);
+                endTimeVar.setValue(externalEndTime);
+
+                // Import the external components into Fmi2API
+                Map<String, ComponentVariableFmi2Api> fmuInstances =
+                        FromMaBLToMaBLAPI.GetComponentVariablesFrom(builder, formalArguments.get(0), env);
+
+                // Create bindings
+                FromMaBLToMaBLAPI.CreateBindings(fmuInstances, env);
+
+                // Create the logging
+                DataWriter dataWriter = builder.getMablToMablAPI().getDataWriter();
+                DataWriter.DataWriterInstance dataWriterInstance = dataWriter.createDataWriterInstance();
+                dataWriterInstance
+                        .initialize(fmuInstances.values().stream().flatMap(x -> x.getVariablesToLog().stream()).collect(Collectors.toList()));
+
+
+                // Create the iteration predicate
+                PredicateFmi2Api loopPredicate = currentCommunicationTime.toMath().addition(stepSizeVar).lessThan(endTimeVar);
+                DoubleVariableFmi2Api absTol = dynamicScope.store("absolute_tolerance", 1.0);
+                DoubleVariableFmi2Api relTol = dynamicScope.store("relative_tolerance", 1.0);
+
+
+            } catch (Exception e) {
+                throw new ExpandException("Internal error: ", e);
+            }
+        } else {
+            throw new ExpandException("Unknown function");
+        }
+
+        return null;
+    }
+
+
 
     @Override
     public boolean requireConfig() {
