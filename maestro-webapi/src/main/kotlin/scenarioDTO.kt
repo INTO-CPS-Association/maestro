6f024f2f781deb13d37105f18130717fdfc64719
FrederikPM <frdrkpm@gmail.com>
2021-05-19 15:12:11 +0200

    Update scenarioDTO.kt
    Refactored to use string

--- maestro-webapi/src/main/kotlin/scenarioDTO.kt
+++ maestro-webapi/src/main/kotlin/scenarioDTO.kt
@@ -4,23 +4,24 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.intocps.maestro.webapi.maestro2.dto.MultiModelScenarioVerifier
 
 data class FmuDTO(
-    @JsonProperty("canRejectStep") val canRejectStep: Boolean,
+    @JsonProperty("can-reject-step") val canRejectStep: Boolean,
     @JsonProperty("inputs") val inputs: Map<String, InputDTO>,
     @JsonProperty("outputs") val outputs: Map<String, OutputDTO>
 )
 
-data class InputDTO(@JsonProperty("reactive") val reactive: Boolean)
+data class InputDTO(@JsonProperty("reactivity") val reactive: Boolean)
 
-data class OutputDTO(@JsonProperty("dependenciesInit") val dependenciesInit: List<String>, @JsonProperty("dependencies") val dependencies: List<String>)
+data class OutputDTO(@JsonProperty("dependencies-init") val dependenciesInit: List<String>, @JsonProperty("dependencies") val dependencies: List<String>)
 
 data class ScenarioDTO(@JsonProperty("fmus") val fmus: Map<String, FmuDTO>, @JsonProperty("connections") val connections: List<String>, @JsonProperty("maxPossibleStepSize") val maxPossibleStepSize: Int)
 
 data class MasterModelDTO(
-    @JsonProperty("scenario") val scenario: ScenarioDTO?,
-    @JsonProperty("instantiation") val instantiation: List<String>?,
-    @JsonProperty("initialization") val initialization: List<String>,
-    @JsonProperty("coSimStep") val coSimStep: List<String>,
-    @JsonProperty("terminate") val terminate: List<String>?
+    @JsonProperty("name") val name: String?,
+    @JsonProperty("scenario") val scenario: String?,
+    @JsonProperty("instantiation") val instantiation: String?,
+    @JsonProperty("initialization") val initialization: String?,
+    @JsonProperty("coSimStep") val coSimStep: String?,
+    @JsonProperty("terminate") val terminate: String?,
 )
 
-data class MasterModelWithMultiModelDTO(@JsonProperty("masterModel") val masterModel: MasterModelDTO, @JsonProperty("multiModel") val multiModel: MultiModelScenarioVerifier)
+data class MasterModelWithMultiModelDTO(@JsonProperty("masterModel") val masterModel: String, @JsonProperty("multiModel") val multiModel: MultiModelScenarioVerifier)
