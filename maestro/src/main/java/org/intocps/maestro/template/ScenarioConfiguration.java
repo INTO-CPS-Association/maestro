986e312ae80bd1aea62bcfbe57a8c6e331da3efd
FrederikPM <frdrkpm@gmail.com>
2021-05-31 11:45:08 +0200

    Update ScenarioConfiguration.java
    Added variables related to execution

--- maestro/src/main/java/org/intocps/maestro/template/ScenarioConfiguration.java
+++ maestro/src/main/java/org/intocps/maestro/template/ScenarioConfiguration.java
@@ -3,13 +3,24 @@ package org.intocps.maestro.template;
 import core.MasterModel;
 import org.intocps.maestro.framework.fmi2.Fmi2SimulationEnvironment;
 
+import java.util.Map;
+
 public class ScenarioConfiguration {
     private final Fmi2SimulationEnvironment simulationEnvironment;
     private final MasterModel masterModel;
+    private final Map<String, Object> parameters;
+    private final Double convergenceRelativeTolerance;
+    private final Double convergenceAbsoluteTolerance;
+    private final Integer convergenceAttempts;
 
-    public ScenarioConfiguration(Fmi2SimulationEnvironment simulationEnvironment, MasterModel masterModel){
+    public ScenarioConfiguration(Fmi2SimulationEnvironment simulationEnvironment, MasterModel masterModel, Map<String, Object> parameters,
+            Double convergenceRelativeTolerance, Double convergenceAbsoluteTolerance, Integer convergenceAttempts){
         this.simulationEnvironment = simulationEnvironment;
         this.masterModel = masterModel;
+        this.parameters = parameters;
+        this.convergenceRelativeTolerance = convergenceRelativeTolerance;
+        this.convergenceAbsoluteTolerance = convergenceAbsoluteTolerance;
+        this.convergenceAttempts = convergenceAttempts;
     }
 
     public Fmi2SimulationEnvironment getSimulationEnvironment() {
@@ -18,5 +29,21 @@ public class ScenarioConfiguration {
 
     public MasterModel getMasterModel() {
         return masterModel;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public Double getConvergenceRelativeTolerance() {
+        return convergenceRelativeTolerance;
+    }
+
+    public Double getConvergenceAbsoluteTolerance() {
+        return convergenceAbsoluteTolerance;
+    }
+
+    public Integer getConvergenceAttempts() {
+        return convergenceAttempts;
     }
 }
