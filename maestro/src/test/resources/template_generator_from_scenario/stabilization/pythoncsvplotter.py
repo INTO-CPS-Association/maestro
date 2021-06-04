7ac2147397adc96ffc732b22d51e0dc8d374fbb1
FrederikPM <frdrkpm@gmail.com>
2021-06-04 11:43:26 +0200

    Delete pythoncsvplotter.py

--- maestro/src/test/resources/template_generator_from_scenario/stabilization/pythoncsvplotter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv("expectedoutputs.csv")
-plt.plot(df['time'], df['m1.m1_mi1.v1'], label="m1_mi1.v1")
-plt.plot(df['time'], df['m2.m2_mi2.fk'], label="m2_mi2.fk")
-plt.plot(df['time'], df['m1.m1_mi1.x1'], label="m1_mi1.x1")
-plt.legend()
-
-plt.show()
