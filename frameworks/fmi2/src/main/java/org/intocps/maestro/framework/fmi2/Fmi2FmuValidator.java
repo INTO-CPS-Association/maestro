836bff949ea08d2df7271fae0058570afe04ed59
FrederikPM <frdrkpm@gmail.com>
2021-07-02 08:15:21 +0200

    FmuValidator changed to utilize the overloaded MaestroCheck check method
    The following error needs to be resolved:
    
    VDM schema does not contain type qName ModelHierarchy
    FmiModelDescription
     VendorAnnotations
      Tool

--- frameworks/fmi2/src/main/java/org/intocps/maestro/framework/fmi2/Fmi2FmuValidator.java
+++ frameworks/fmi2/src/main/java/org/intocps/maestro/framework/fmi2/Fmi2FmuValidator.java
@@ -10,9 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 public class Fmi2FmuValidator implements IFmuValidator {
@@ -23,32 +20,19 @@ public class Fmi2FmuValidator implements IFmuValidator {
         logger.warn("Fmi2FmuValidator disabled");
         try {
             logger.trace("Validating: {} at {}", id, path);
-
             IFmu fmu = FmuFactory.create(null, path);
-
-            File tempDir = Files.createTempDirectory("ModelDescriptionDirTemp").toFile();
-            tempDir.deleteOnExit();
-
-            File modelDescriptionXML = Path.of(tempDir.toString(), "modelDescription.xml").toFile();
-
-            java.nio.file.Files.copy(
-                    fmu.getModelDescription(),
-                    modelDescriptionXML.toPath(),
-                    StandardCopyOption.REPLACE_EXISTING);
-
             MaestroCheck checker = new MaestroCheck();
-            List<OnFailError> onFailErrors = checker.check(modelDescriptionXML);
+            List<OnFailError> onFailErrors = checker.check(fmu.getModelDescription());
 
             onFailErrors.forEach(onFailError -> {
-                reporter.warning(onFailError.errno, onFailError.message, new LexToken(path + File.separator + "modelDescription" + ".xml", 0,
-                        0));
+                reporter.warning(onFailError.errno, onFailError.message, new LexToken(path + File.separator + "modelDescription" + ".xml", 0, 0));
                 logger.trace(onFailError.toString());
             });
 
             return onFailErrors.isEmpty();
 
         } catch (Exception e) {
-            logger.error("An unknown exception occured during Fmi2FmUValidator: ", e);
+            logger.error("An unknown exception occurred during Fmi2FmUValidator: ", e);
             return false;
         }
     }
--- frameworks/fmi2/src/test/java/FmuValidatorTest.java
+++ frameworks/fmi2/src/test/java/FmuValidatorTest.java
@@ -5,17 +5,19 @@ import org.intocps.maestro.framework.fmi2.IFmuValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Objects;
 
 public class FmuValidatorTest {
 
     @Test
-    public void validateTest() throws URISyntaxException {
+    public void validateTest() {
+        // Arrange
         IFmuValidator fmuValidator = new Fmi2FmuValidator();
         URL fmuUrl = FmuValidatorTest.class.getClassLoader().getResource("watertankcontroller-c.fmu");
         IErrorReporter errorReporter = new ErrorReporter();
+
+        // Assert
         Assertions.assertDoesNotThrow(() -> fmuValidator.validate("1", Objects.requireNonNull(fmuUrl).toURI(), errorReporter));
         Assertions.assertEquals(2, errorReporter.getWarnings().size());
     }
