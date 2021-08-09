26a851b54948008a5e3e2ee0350ed2c6ca18f1e3
Kenneth Lausdahl <Kenneth.GuldbrandtLausdahl@agcocorp.com>
2021-08-09 16:01:55 +0200

    added try to mabl factory

---
+++ frameworks/fmi2api/src/main/java/org/intocps/maestro/framework/fmi2/api/mabl/scoping/TryMaBlScope.java
@@ -0,0 +1,38 @@
+package org.intocps.maestro.framework.fmi2.api.mabl.scoping;
+
+import org.intocps.maestro.ast.node.PStm;
+import org.intocps.maestro.framework.fmi2.api.Fmi2Builder;
+import org.intocps.maestro.framework.fmi2.api.mabl.MablApiBuilder;
+
+public class IfMaBlScope implements Fmi2Builder.IfScope<PStm> {
+    private final MablApiBuilder builder;
+    private final PStm declaration;
+    private final ScopeFmi2Api declaringScope;
+    private final ScopeFmi2Api thenScope;
+    private final ScopeFmi2Api elseScope;
+
+    public IfMaBlScope(MablApiBuilder builder, PStm declaration, ScopeFmi2Api declaringScope, ScopeFmi2Api thenScope, ScopeFmi2Api elseScope) {
+        this.builder = builder;
+        this.declaration = declaration;
+        this.declaringScope = declaringScope;
+        this.thenScope = thenScope;
+        this.elseScope = elseScope;
+
+        enterThen();
+    }
+
+    @Override
+    public ScopeFmi2Api enterThen() {
+        return thenScope.activate();
+    }
+
+    @Override
+    public ScopeFmi2Api enterElse() {
+        return elseScope.activate();
+    }
+
+    @Override
+    public ScopeFmi2Api leave() {
+        return declaringScope.activate();
+    }
+}
