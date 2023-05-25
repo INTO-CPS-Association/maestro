281a938ca47a5b37297a197e8f85162396a0c800
Kenneth Lausdahl <kenneth@lausdahl.com>
2023-05-25 13:09:39 +0200

    added refernce fmus for fmi3

--- maestro/src/test/java/org/intocps/maestro/Fmi3ModuleTest.java
+++ maestro/src/test/java/org/intocps/maestro/Fmi3ModuleTest.java
@@ -18,7 +18,7 @@ import java.util.stream.Stream;
 public class Fmi3ModuleTest extends FullSpecTest {
 
     private static Stream<Arguments> data() {
-        return Arrays.stream(Objects.requireNonNull(Paths.get("src", "test", "resources", "fmi3").toFile().listFiles()))
+        return Arrays.stream(Objects.requireNonNull(Paths.get("src", "test", "resources", "fmi3","basic").toFile().listFiles()))
                 .filter(n -> !n.getName().startsWith(".")).map(f -> Arguments.arguments(f.getName(), f));
     }
 
