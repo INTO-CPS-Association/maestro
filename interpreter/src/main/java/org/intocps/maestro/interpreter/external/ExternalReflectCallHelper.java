ad798d72d06ca803dde9f3f17db5e67a6fc7d60e
kel <kenneth@lausdahl.com>
2024-09-16 11:47:14 +0200

    fixed issue with long generating real value

--- interpreter/src/main/java/org/intocps/maestro/interpreter/external/ExternalReflectCallHelper.java
+++ interpreter/src/main/java/org/intocps/maestro/interpreter/external/ExternalReflectCallHelper.java
@@ -373,7 +373,7 @@ public class ExternalReflectCallHelper extends Vector<IArgMapping> {
                     }
                     case Long: {
                         var n = ((NumericValue) v);
-                        return n.realValue();// Integer.valueOf(n.intValue()).longValue();
+                        return n.longValue();// Integer.valueOf(n.intValue()).longValue();
                     }
                     case Real: {
                         var n = ((NumericValue) v);
