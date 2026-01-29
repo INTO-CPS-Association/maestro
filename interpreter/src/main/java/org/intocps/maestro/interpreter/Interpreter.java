df1dd15733010735bd268f3536d67ff7634b2a97
Kenneth Lausdahl <kenneth@lausdahl.com>
2026-01-29 10:32:15 +0100

    updates for new debug syntax, fmi3 events added debug context printing

--- interpreter/src/main/java/org/intocps/maestro/interpreter/Interpreter.java
+++ interpreter/src/main/java/org/intocps/maestro/interpreter/Interpreter.java
@@ -73,7 +73,7 @@ public class Interpreter extends QuestionAnswerAdaptor<Context, Value> {
 
     @Override
     public Value caseASimulationSpecificationCompilationUnit(ASimulationSpecificationCompilationUnit node,
-            Context question) throws AnalysisException {
+                                                             Context question) throws AnalysisException {
         return node.getBody().apply(this, question);
 
     }
@@ -383,7 +383,7 @@ public class Interpreter extends QuestionAnswerAdaptor<Context, Value> {
             if (v instanceof NullValue) {
                 logger.error("The target object: \"" + node.getObject().toString() + "\" is null. Related call: \"" + node + "\"");
                 throw new InterpreterException("Unhandled node: " + node);
-            }else if(!(v instanceof ModuleValue)){
+            } else if (!(v instanceof ModuleValue)) {
                 logger.error("The target object: \"" + node.getObject().toString() + "\" is not a module. Related call: \"" + node + "\"");
                 throw new InterpreterException("Unhandled node: " + node);
             } else {
@@ -400,7 +400,7 @@ public class Interpreter extends QuestionAnswerAdaptor<Context, Value> {
             } catch (InterpreterTransitionException te) {
                 throw te;
             } catch (Exception e) {
-                throw new InterpreterException("Unable to evaluate node: " + node + " at "+node.getLocation(), e);
+                throw new InterpreterException("Unable to evaluate node: " + node + " at " + node.getLocation(), e);
             }
         }
 
@@ -451,7 +451,7 @@ public class Interpreter extends QuestionAnswerAdaptor<Context, Value> {
                 message = msg.toString();
             }
 
-            message +=" \""+node.getExp().getLocation().toString()+"\"";
+            message += " \"" + node.getExp().getLocation().toString() + "\"";
         }
 
         throw new ErrorException(message);
@@ -658,22 +658,31 @@ public class Interpreter extends QuestionAnswerAdaptor<Context, Value> {
     @Override
     public Value caseADebugStm(ADebugStm node, Context question) throws AnalysisException {
 
-        var tokens = node.getTokens()==null?Collections.emptyList():node.getTokens().stream().map(AStringLiteralExp::getValue).toList();
-        if(tokens.contains("context"))
-        {
-            Context ctxt= question;
+        var tokens = node.getTokens() == null ? Collections.emptyList() : node.getTokens().stream().map(AStringLiteralExp::getValue).toList();
+        if (tokens.contains("context")) {
+            Context ctxt = question;
             Stack<Context> stack = new Stack<>();
             stack.push(ctxt);
-            while((ctxt=ctxt.outer)!=null){stack.push(ctxt);}
+            while ((ctxt = ctxt.outer) != null) {
+                stack.push(ctxt);
+            }
             StringBuilder sb = new StringBuilder();
             sb.append("Context stack:\n");
-            while(!stack.isEmpty()){
-                var depth = stack.size();
-                var frame = stack.pop();
-                var prefix = Stream.generate(() -> "\t").limit(depth-1).collect(Collectors.joining());
-                for(var entry:frame.values.entrySet())
+            int depth = 0;
+            for (var frame : stack.reversed()) {
+                var prefix = Stream.generate(() -> "\t").limit(depth++).collect(Collectors.joining());
+                if (depth > 1) {
+                    sb.append("\n");
+                }
+                sb.append(prefix).append("## Context depth: ").append(depth).append(" ##\n");
+                if(frame.values.isEmpty())
                 {
-                    sb.append(prefix).append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                    sb.append(prefix).append("\t").append("-- empty --").append("\n");
+                }
+                for (var entry : frame.values.entrySet().stream()
+                        .sorted(Comparator.comparing(e -> e.getKey().getText()))
+                        .toList()) {
+                    sb.append(prefix).append("\t").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
                 }
             }
 
